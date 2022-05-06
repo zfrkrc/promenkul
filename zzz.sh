aa6c8eae87a0a3396897098ff7cdb6d25a95168a
zfrkrc <zfrkrc@gmail.com>
2022-05-06 21:32:09 +0300

    script

--- zzz.sh
+++ zzz.sh
@@ -1,7 +1,12 @@
 git pull
 docker ps -a
-docker rm pro
-
-docker image rm pro nginx -f
 docker build . -t pro
+docker rm pro -f
 docker run -d -p 80:80 --name pro pro
+docker image prune -f
+clear
+echo "Servis"
+docker ps -a
+echo "Diskler"
+docker image ls -a
+echo "iþ tamam?"
