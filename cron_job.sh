46c55cd79c342f7614a34b45a627d4fdb1b8631e
zfrkrc <zfrkrc@gmail.com>
2023-03-09 22:48:31 +0300

    new filw adddd

---
+++ cron_job.sh
@@ -0,0 +1,12 @@
+# cleanup exited docker containers
+EXITED_CONTAINERS=$(docker ps -a | grep Exited | awk '{ print $1 }')
+if [ -z "$EXITED_CONTAINERS" ]
+then
+        echo "No exited containers to clean"
+else
+        docker rm $EXITED_CONTAINERS
+fi
+ 
+# renew certbot certificate
+docker-compose -f /root/matomo/docker-compose.yaml run --rm certbot
+docker-compose -f /root/matomo/docker-compose.yaml exec nginx nginx -s reload
